8fca23745aff662ca2e58f969e954bca5f875244
Piet Dijkmans <piet.dijkmans@gmail.com>
2026-03-16 16:47:41 +0100

    feat: add fetchBoxes api function

---
+++ web/src/features/boxes/api/getBoxes.js
@@ -0,0 +1,20 @@
+import { db } from "../../../lib/firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+/**
+ * Haalt alle gridboxen op uit Firestore
+ */
+export const fetchBoxes = async () => {
+  try {
+    const boxesCol = collection(db, "boxes");
+    const boxSnapshot = await getDocs(boxesCol);
+    const boxList = boxSnapshot.docs.map(doc => ({
+      id: doc.id,
+      ...doc.data()
+    }));
+    return boxList;
+  } catch (error) {
+    console.error("Fout bij het ophalen van boxen:", error);
+    throw error;
+  }
+};
