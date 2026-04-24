77e0adb5040bddcf5d303173ea888e1ff04f6755
Piet Dijkmans <piet.dijkmans@gmail.com>
2026-04-24 04:02:25 +0200

    fix: voeg DBUS_SESSION_BUS_ADDRESS toe voor rpi-connect in update.sh

--- update.sh
+++ update.sh
@@ -18,10 +18,10 @@ echo "[UPDATE] Linger inschakelen voor gebruiker pi..."
 loginctl enable-linger pi
 
 echo "[UPDATE] rpi-connect user service inschakelen..."
-sudo -u pi XDG_RUNTIME_DIR=/run/user/1000 systemctl --user enable rpi-connect
+sudo -u pi DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" XDG_RUNTIME_DIR=/run/user/1000 systemctl --user enable rpi-connect
 
 echo "[UPDATE] rpi-connect starten..."
-sudo -u pi XDG_RUNTIME_DIR=/run/user/1000 rpi-connect on
+sudo -u pi DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus" XDG_RUNTIME_DIR=/run/user/1000 rpi-connect on
 
 echo "[UPDATE] gridbox.service herstarten..."
 systemctl restart gridbox.service
